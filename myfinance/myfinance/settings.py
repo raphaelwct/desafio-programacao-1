5a97d57b79f2780fb9740e2fa75433deb3f5424a
Raphael Carvalho <raphael.carvalho@acotel.com>
2014-02-14 13:45:51 -0200

    Configurando diretorio de templates.

--- myfinance/myfinance/settings.py
+++ myfinance/myfinance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIR = (os.path.join(BASE_DIR, 'purchase', 'templates'),)
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'purchase',
 )
 
 MIDDLEWARE_CLASSES = (
