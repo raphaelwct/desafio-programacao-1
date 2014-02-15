1cf41b58170b246f5831d547e05c49e6f995e438
Raphael Carvalho <raphaelwct@gmail.com>
2014-02-15 11:09:07 -0200

    Finalizando implementacao dos steps do teste de aceitacao.

--- myfinance/purchase/tests/features/purchase_data_importer.feature
+++ myfinance/purchase/tests/features/purchase_data_importer.feature
@@ -5,11 +5,11 @@
 Funcionalidade: Importar dados de vendas
 
     Contexto: 
-        Dado que eu acesso o formulário de importação
+        Dado que eu acesso o formulario de importacao
 
 
-    Cenário: Operação realizada com sucesso
+    Cenário: Operacao realizada com sucesso
 
-        Dado que eu faço o upload de um arquivo
-        Quando eu pressiono o botão importar
-        Então eu devo ver "Importação realizada com sucesso"
+        Dado que eu faco o upload de um arquivo
+        Quando eu pressiono o botao importar
+        Então eu devo ver "Importacao realizada com sucesso"
--- myfinance/purchase/tests/features/steps/purchase_data_importer.py
+++ myfinance/purchase/tests/features/steps/purchase_data_importer.py
@@ -5,11 +5,11 @@ from nose import tools
 from django.conf import settings
 import os
 
-@given(u'que eu acesso o formulário de importação')
+@given(u'que eu acesso o formulario de importacao')
 def that_i_access_the_importer_form(context):
     context.browser.get('http://127.0.0.1:8000/purchase/pdi')
 
-@given(u'que eu faço o upload de um arquivo')
+@given(u'que eu faco o upload de um arquivo')
 def that_i_upload_a_purchase_file(context):
     purchase_file_field = context.browser.find_element_by_name('purchase_file')
     tools.assert_true(purchase_file_field)
@@ -17,12 +17,12 @@ def that_i_upload_a_purchase_file(context):
     purchase_file_path = '/home/rcarvalho/Workspace/desafio-programacao-1/example_input.tab'
     purchase_file_field.send_keys(purchase_file_path)
 
-@when('eu pressiono o botão importar')
+@when(u'eu pressiono o botao importar')
 def i_press_the_import_button(context):
     import_button = context.browser.find_element_by_name('import_button')
     import_button.click()
 
-@then('eu devo ver "{message}"')
+@then(u'eu devo ver "{message}"')
 def i_must_see_the_message(context, message):
-    import ipdb;ipdb.set_trace();
     message_field = context.browser.find_element_by_id('message')
+    tools.assert_equals(message, message_field.text)
