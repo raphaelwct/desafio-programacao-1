c38caf1294451c5c2bfd0b165cc5d2e3dc002bf3
Raphael Carvalho <raphaelwct@gmail.com>
2014-02-16 19:14:59 -0300

    Preparando view purchase_importer_form para efetuar a importacao de dados quando receber requisicoes via post.

--- myfinance/purchase/tests/tests.py
+++ myfinance/purchase/tests/tests.py
@@ -1,5 +1,5 @@
-from django.test import TestCase
-import views
+from django.test import TestCase, Client
+from purchase import views
 import mock
 
 
@@ -8,5 +8,19 @@ class ViewsTestCase(TestCase):
     def test_import_data_should_return_message(self):
         request_mock = mock.Mock()
         message_received = views.import_data(request_mock)
-        self.assertTrue(instanceof(message_received, 'str'))
-        self.assertTrue(len(message_received) > 0)
+        self.assertIsInstance(message_received, str)
+        self.assertGreater(len(message_received), 0)
+
+    @mock.patch.object(views, 'import_data')
+    def test_if_purchase_importer_form_calls_import_data_in_case_of_post_request(self, import_data_mock):
+        client = Client()
+        response = client.post('/purchase/pdi/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(import_data_mock.called)
+
+    @mock.patch.object(views, 'import_data')
+    def test_purchase_importer_form_should_not_calls_import_data_in_case_of_get_request(self, import_data_mock):
+        client = Client()
+        response = client.get('/purchase/pdi/')
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(import_data_mock.called)
--- myfinance/purchase/views.py
+++ myfinance/purchase/views.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render_to_response
 
+
 def purchase_importer_form(request):
-    return render_to_response('purchase_importer_form.html')
+    import_message = None
+    if request.method == 'POST':
+        import_message = import_data(request)
+    return render_to_response('purchase_importer_form.html', {'message': import_message})
+
+
+def import_data(request):
+    return 'Importacao efetuada com sucesso'
