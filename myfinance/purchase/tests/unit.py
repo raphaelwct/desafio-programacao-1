92467b596820665044ae0cc4151c5a31e58b66bd
Raphael Carvalho <raphaelwct@gmail.com>
2014-02-16 18:44:35 -0300

    Iniciando unittests para a view import.

--- myfinance/purchase/tests/unit.py
+++ myfinance/purchase/tests/unit.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import views
+import mock
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+
+    def test_import_data_should_return_message(self):
+        request_mock = mock.Mock()
+        message_received = views.import_data(request_mock)
+        self.assertTrue(instanceof(message_received, 'str'))
+        self.assertTrue(len(message_received) > 0)
