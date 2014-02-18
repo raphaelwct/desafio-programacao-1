56a89838606f19b1acc9e60a1ba737b22ce8a792
Raphael Carvalho <raphael.carvalho@acotel.com>
2014-02-18 18:39:07 -0300

    Removendo modulo de admin default do django e configurando databasename.

--- myfinance/myfinance/settings.py
+++ myfinance/myfinance/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -60,7 +59,7 @@ WSGI_APPLICATION = 'myfinance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'pdi.db'),
     }
 }
 
--- myfinance/myfinance/urls.py
+++ myfinance/myfinance/urls.py
@@ -1,13 +1,6 @@
 from django.conf.urls import patterns, include, url
 from purchase import urls as purchase_urls
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myfinance.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^purchase/', include(purchase_urls)),
 )
