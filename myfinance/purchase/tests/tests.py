a36af89785efdcc804baf4f9d19563ffe7c171d2
Raphael Carvalho <raphael.carvalho@acotel.com>
2014-02-19 15:40:54 -0300

    Corrigindo testes de acordo com o refactor feito anteriomente.

--- myfinance/purchase/tests/tests.py
+++ myfinance/purchase/tests/tests.py
@@ -39,42 +39,64 @@ class PurchaseImporterFormViewTestCase(TestCase):
 
 class ImportDataViewTestCase(TestCase):
 
+    @mock.patch.object(views, 'calc_purchase_total')
+    @mock.patch.object(views, 'save_purchase_data')
+    @mock.patch.object(views, 'read_file_lines')
     @mock.patch.object(views, 'parse_purchase_file_data')
     @mock.patch.object(models, 'Purchase')
-    @mock.patch.object(views, 'normalize_data')
     def test_import_data_should_return_a_save_feedback_message_and_the_purchase_total(self,
-            normalize_data, purchase_model_mock, parse_purchase_file_data_mock):
-        fake_parsed_line = iter([('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St',
-                'Bobs Pizza')])
+            purchase_model_mock, parse_purchase_file_data_mock, read_file_lines_mock,
+            save_purchase_data_mock, calc_purchase_total_mock):
         request_mock = mock.Mock()
         request_mock.FILES = {'purchase_file': mock.Mock()}
-        parse_purchase_file_data_mock.return_value = fake_parsed_line
+
+        fake_file_line = iter([
+            ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
+        ])
+        read_file_lines_mock.return_value = fake_file_line
+
+        calc_purchase_total_mock.return_value = 20
+
         messages = views.import_data(request_mock)
+
         self.assertIn('import_feedback', messages)
         self.assertIn('purchase_total', messages)
-        self.assertGreater(len(messages['import_feedback']), 0)
-        self.assertGreater(len(messages['purchase_total']), 0)
 
-    @mock.patch.object(views, 'parse_purchase_file_data')
-    def test_import_data_should_call_parse_purchase_file_data(self, parse_purchase_file_data_mock):
-        request_mock = mock.Mock()
-        request_mock.FILES = {'purchase_file': mock.Mock()}
-        views.import_data(request_mock)
-        self.assertTrue(parse_purchase_file_data_mock.called)
+        self.assertEquals(messages['import_feedback'], 'Importacao efetuada com sucesso.')
+        self.assertEquals(messages['purchase_total'], 'A receita bruta total foi de R$ 20.')
 
-    @mock.patch.object(views, 'parse_purchase_file_data')
+    @mock.patch.object(views, 'calc_purchase_total')
     @mock.patch.object(views, 'save_purchase_data')
-    def test_import_data_should_call_save_purchase_data(self, save_purchase_data_mock,
-            parse_purchase_file_data_mock):
+    @mock.patch.object(views, 'read_file_lines')
+    @mock.patch.object(views, 'parse_purchase_file_data')
+    def test_import_data_integration(self, parse_purchase_file_data_mock,
+            read_file_lines_mock, save_purchase_data_mock, calc_purchase_total_mock):
         request_mock = mock.Mock()
-        request_mock.FILES = {'purchase_file': mock.Mock()}
-        file_line1 = ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
-        file_line2 = ('Homer', 'R$50 off R$ 20 of beer', '5.0', '4', '111 Fake St', 'Beer Taste')
-        fake_parsed_line = iter([file_line1, file_line2])
-        parse_purchase_file_data_mock.return_value = fake_parsed_line
+        fake_purchase_file = StringIO()
+        request_mock.FILES = {'purchase_file': fake_purchase_file}
+
+        file_line = ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
+        read_file_lines_mock.return_value = iter([file_line])
+
+        parsed_data = {
+            'purchaser_name': 'Joao Silva',
+            'item_description': 'R$10 off R$ 20 of food',
+            'item_price': 10.0,
+            'purchase_count': 2,
+            'merchant_address': '987 Fake St',
+            'merchant_name': 'Bobs Pizza'
+        }
+        parse_purchase_file_data_mock.return_value = parsed_data
+
         views.import_data(request_mock)
-        excepted_calls = [mock.call(file_line1), mock.call(file_line2)]
-        save_purchase_data_mock.assert_has_calls(excepted_calls, any_order=True)
+
+        read_file_lines_mock.assert_called_with(fake_purchase_file)
+
+        parse_purchase_file_data_mock.assert_called_with(file_line)
+
+        save_purchase_data_mock.assert_called_with(parsed_data)
+
+        calc_purchase_total_mock.assert_called_with(parsed_data)
 
 
 class ParsePurchaseFileDataViewTestCase(TestCase):
@@ -94,77 +116,46 @@ class ParsePurchaseFileDataViewTestCase(TestCase):
 
 class SavePurchaseDataViewTestCase(TestCase):
 
+    @mock.patch.object(models.Merchant.objects, 'get_or_create')
+    @mock.patch.object(models.Item.objects, 'get_or_create')
+    @mock.patch.object(models.Purchaser.objects, 'get_or_create')
     @mock.patch.object(models, 'Purchase')
-    @mock.patch.object(views, 'normalize_data')
-    def test_save_purchase_data_must_call_normalize_data_method(self, normalize_data_mock,
-            purchase_model_mock):
-        views.save_purchase_data(mock.Mock())
-        self.assertTrue(normalize_data_mock.called)
+    def test_save_purchase_data_should_save_all_the_file_data_in_models_which_represents_a_purchase(self,
+            purchase_model_mock, purchaser_create_mock, item_create_mock, merchant_create_mock):
+        # Mocking all models
+        purchaser_object = mock.Mock()
+        purchaser_create_mock.return_value = [purchaser_object]
+        item_object = mock.Mock()
+        item_create_mock.return_value = [item_object]
+        merchant_object = mock.Mock()
+        merchant_create_mock.return_value = [merchant_object]
+        purchase_object_mock = mock.Mock()
+        purchase_model_mock.return_value = purchase_object_mock
+        purchase_count = 2
 
-    @mock.patch.object(models, 'Purchase')
-    @mock.patch.object(views, 'normalize_data')
-    def test_save_purchase_data_should_save_all_the_normalized_data_in_a_model_that_represents_a_purchase(self,
-            normalize_data_mock, purchase_model_mock):
-        purchaser_mock = mock.Mock()
-        item_mock = mock.Mock()
-        merchant_mock = mock.Mock()
-        purchase_count = 10
-        normalize_data_mock.return_value = {
-            'purchaser': purchaser_mock,
-            'item': item_mock,
-            'merchant': merchant_mock,
-            'purchase_count': purchase_count
+        parsed_data = {
+            'purchaser_name': 'Joao Silva',
+            'item_description': 'R$10 off R$ 20 of food',
+            'item_price': 10.0,
+            'purchase_count': purchase_count,
+            'merchant_address': '987 Fake St',
+            'merchant_name': 'Bobs Pizza'
         }
-        views.save_purchase_data(mock.Mock())
-        purchase_model_mock.assert_called_with(purchaser=purchaser_mock, item=item_mock,
-                merchant=merchant_mock, count=purchase_count)
+        views.save_purchase_data(parsed_data)
 
+        purchaser_create_mock.assert_called_with(name=parsed_data['purchaser_name'])
 
-class NormalizaDataViewTestCase(TestCase):
+        item_create_mock.assert_called_with(
+            description=parsed_data['item_description'],
+            price=parsed_data['item_price']
+        )
 
-    def test_normalize_data_must_return_instances_of_purchase_models(self):
-        fake_parsed_line = ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
-        normalized_data = views.normalize_data(fake_parsed_line)
-        self.assertIn('purchaser', normalized_data)
-        self.assertIn('item', normalized_data)
-        self.assertIn('merchant', normalized_data)
-        self.assertIsInstance(normalized_data['purchaser'], models.Purchaser)
-        self.assertIsInstance(normalized_data['item'], models.Item)
-        self.assertIsInstance(normalized_data['merchant'], models.Merchant)
+        merchant_create_mock.assert_called_with(
+            name=parsed_data['merchant_name'],
+            address=parsed_data['merchant_address']
+        )
 
-    def test_normalize_data_must_convert_the_data_lines_in_data_models(self):
-        fake_parsed_line = ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
-        normalized_data = views.normalize_data(fake_parsed_line)
+        self.assertTrue(purchase_object_mock.save.called)
 
-        purchaser = normalized_data['purchaser']
-        item = normalized_data['item']
-        merchant = normalized_data['merchant']
-
-        self.assertEquals(purchaser.name, 'Joao Silva')
-
-        self.assertEquals(item.description, 'R$10 off R$ 20 of food')
-        self.assertEquals(item.price, 10.0)
-
-        self.assertEquals(merchant.address, '987 Fake St')
-        self.assertEquals(merchant.name, 'Bobs Pizza')
-
-        self.assertEquals(normalized_data['purchase_count'], 2)
-
-    @mock.patch.object(models.Merchant, 'objects')
-    @mock.patch.object(models.Item, 'objects')
-    @mock.patch.object(models.Purchaser, 'objects')
-    @mock.patch('purchase.models.Purchase')
-    def test_normalize_data_must_save_some_of_the_normalized_models(self, purchase_model_mock,
-            purchaser_manager_mock, item_manager_mock, merchant_manager_mock):
-        fake_parsed_line = ('Joao Silva', 'R$10 off R$ 20 of food', '10.0', '2', '987 Fake St', 'Bobs Pizza')
-        views.normalize_data(fake_parsed_line)
-        self.assertTrue(purchaser_manager_mock.get_or_create.called)
-        self.assertTrue(item_manager_mock.get_or_create.called)
-        self.assertTrue(merchant_manager_mock.get_or_create.called)
-
-
-    def test_normalize_data_should_cast_to_int_the_purchaser_count(self):
-        pass
-
-    def test_normalize_data_should_cast_to_float_the_item_price(self):
-        pass
+        purchase_model_mock.assert_called_with(purchaser=purchaser_object, item=item_object,
+            merchant=merchant_object, count=purchase_count)
